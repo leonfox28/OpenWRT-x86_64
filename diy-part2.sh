c30108359681b2b859f4a2745e7844990f2dec96
Leon <41661196+leonfox28@users.noreply.github.com>
2024-08-15 15:20:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,29 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+# Modift default wan port to eth3
+sed -i "s/eth1/eth3/g" ./package/base-files/files/etc/board.d/99-default_network
+
+# clone openclash
+mkdir package/luci-app-openclash
+cd package/luci-app-openclash
+git init
+git remote add -f origin https://github.com/vernesong/OpenClash.git
+git config core.sparsecheckout true
+echo "luci-app-openclash" >> .git/info/sparse-checkout
+git pull --depth 1 origin master
+git branch --set-upstream-to=origin/master master
+# 编译 po2lmo (如果有po2lmo可跳过)
+# pushd luci-app-openclash/tools/po2lmo
+# make && sudo make install
+# popd
+# 回退到主项目目录
+cd ../..
